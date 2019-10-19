a3df746548270b083e08f5566d128fa4bc5c429a
ERICK503 <erick.najera@catolica.edu.sv>
2019-10-18 23:55:26 -0600

    add publicacion y lista

---
+++ app/src/main/java/sv/edu/catolica/bolanios/jonathan/descubreelsalvador/CargarLugares.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.bolanios.jonathan.descubreelsalvador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CargarLugares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargar_lugares);
+    }
+}
