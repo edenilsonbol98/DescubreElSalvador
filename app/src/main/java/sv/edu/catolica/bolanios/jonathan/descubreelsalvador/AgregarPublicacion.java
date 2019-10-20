28430a09f2124a98cede1c90452cf5108f3d5e4d
ERICK503 <erick.najera@catolica.edu.sv>
2019-10-19 21:19:52 -0600

    modales parte 1

--- app/src/main/java/sv/edu/catolica/bolanios/jonathan/descubreelsalvador/AgregarPublicacion.java
+++ app/src/main/java/sv/edu/catolica/bolanios/jonathan/descubreelsalvador/AgregarPublicacion.java
@@ -2,13 +2,42 @@ package sv.edu.catolica.bolanios.jonathan.descubreelsalvador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AgregarPublicacion extends AppCompatActivity {
+
+    Context contexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_publicacion);
+        contexto= this;
+        Button botonAgregarTel = (Button) findViewById(R.id.btnmas1);
+        Button botonAgregarcel = (Button) findViewById(R.id.btnagreCel);
+        Button botoncalendario = (Button) findViewById(R.id.btnCalendario);
+
+
+        botonAgregarTel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Dialogo_telefono(contexto);
+            }
+        });
+        botonAgregarcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Dialogo_telefono(contexto);
+            }
+        });
+        botoncalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Dialogo_telefono(contexto);
+            }
+        });
     }
 }
