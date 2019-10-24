f2a865bdc94733fbbff22ee8e588a982ff4c65fa
edenilsonbol98 <54971667+edenilsonbol98@users.noreply.github.com>
2019-10-23 23:47:51 -0600

    Login con facebook
    Signed-off-by: edenilsonbol98 <54971667+edenilsonbol98@users.noreply.github.com>

--- app/src/main/java/sv/edu/catolica/bolanios/jonathan/descubreelsalvador/PrincipalElSalvador.java
+++ app/src/main/java/sv/edu/catolica/bolanios/jonathan/descubreelsalvador/PrincipalElSalvador.java
@@ -1,16 +1,38 @@
 package sv.edu.catolica.bolanios.jonathan.descubreelsalvador;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PrincipalElSalvador extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private LoginButton logueoFace;
+    private CallbackManager callM;
+    private TextView texto1,texto2;
+    private AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +40,86 @@ public class PrincipalElSalvador extends AppCompatActivity {
         setContentView(R.layout.activity_principal_el_salvador);
 
         mAuth = FirebaseAuth.getInstance();
+logueoFace=findViewById(R.id.btnLoginFacebook);
+callM=CallbackManager.Factory.create();
+texto1=findViewById(R.id.email);
+texto2=findViewById(R.id.nombre);
+ accessToken = AccessToken.getCurrentAccessToken();
 
+
+logueoFace.registerCallback(callM, new FacebookCallback<LoginResult>() {
+    @Override
+    public void onSuccess(LoginResult loginResult) {
 
     }
+
+    @Override
+    public void onCancel() {
+
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+
+    }
+});
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callM.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker=new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken==null){
+                texto1.setText("");
+                texto2.setText("");
+                Toast.makeText(PrincipalElSalvador.this,"Usuario no logueado",Toast.LENGTH_LONG).show();
+            }else {
+                loguearse(currentAccessToken);
+                startActivity(new Intent(PrincipalElSalvador.this,AgregarPublicacion.class));
+
+            }
+        }
+
+        private void loguearse(AccessToken nuevoAccessToken){
+            final GraphRequest request=GraphRequest.newMeRequest(nuevoAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    try {
+                        String first_name=object.getString("first_name");
+                        String last_name=object.getString("last_name");
+                        String email=object.getString("email");
+                        String id=object.getString("id");
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Bundle parameters=new Bundle();
+            parameters.putString("fields","first_name,last_name,email,id");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
+        public void checkLoginStatus(){
+            if(AccessToken.getCurrentAccessToken()!=null){
+                loguearse(AccessToken.getCurrentAccessToken());
+                startActivity(new Intent(PrincipalElSalvador.this,AgregarPublicacion.class));
+            }
+        }
+
+    };
+
+
+
 
     public void irSesion(View view) {
         Intent intencion1 = new Intent(PrincipalElSalvador.this, Login.class);
@@ -30,7 +129,12 @@ public class PrincipalElSalvador extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         if(mAuth.getCurrentUser()!=null){
-            startActivity(new Intent(PrincipalElSalvador.this,CargarLugares.class));
+            startActivity(new Intent(PrincipalElSalvador.this,AgregarPublicacion.class));
+            finish();
+        }
+        if(accessToken!=null){
+            startActivity(new Intent(PrincipalElSalvador.this,AgregarPublicacion.class));
+            finish();
         }
     }
 }
